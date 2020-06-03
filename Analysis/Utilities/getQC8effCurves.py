f520a9704c71511f0007df8db9fbeedbcdd8d0bd
Giovanni Mocellin <giovannimocellin@MBP-di-Giovanni.homenet.telecomitalia.it>
2020-06-03 14:00:02 +0200

    Modified code to get efficiency curve summary plots Now it works for single chambers for which the sister has been turned OFF. The colors of the plots are now well done. Resolution of the plots has been increased.

--- Analysis/Utilities/getQC8effCurves.py
+++ Analysis/Utilities/getQC8effCurves.py
@@ -13,15 +13,15 @@ def getSCname(det_name):
 
     if det_name.startswith("GE11-"):
         det_name = det_name.replace("GE11-","GE1/1-")
-        
+
     db_cond = os.environ["GEM_PRODUCTION_DB_COND"]
     db_name = os.environ["GEM_PRODUCTION_DB_NAME"]
-    
+
     db = cx_Oracle.connect(db_cond+db_name) # production DB
     cur = db.cursor()
     query = "select * from CMS_GEM_MUON_VIEW.GEM_SPRCHMBR_PARTS_VIEW_RH"
     cur.execute(query)
-    
+
     for result in cur:
         if (result[3] == det_name and result[11] == "GEM1"):
             SC_name = str(result[0]).replace('GE1/1-','GE11-')
@@ -31,15 +31,15 @@ def getLayName(SC_name,lay):
 
     if SC_name.startswith("GE11-"):
         SC_name = SC_name.replace("GE11-","GE1/1-")
-        
+
     db_cond = os.environ["GEM_PRODUCTION_DB_COND"]
     db_name = os.environ["GEM_PRODUCTION_DB_NAME"]
-    
+
     db = cx_Oracle.connect(db_cond+db_name) # production DB
     cur = db.cursor()
     query = "select * from CMS_GEM_MUON_VIEW.GEM_SPRCHMBR_PARTS_VIEW_RH"
     cur.execute(query)
-    
+
     for result in cur:
         if (result[0] == SC_name and result[5] == str(lay) and result[11] == "GEM1"):
             lay_name = str(result[3]).replace('GE1/1-','GE11-')
@@ -48,7 +48,7 @@ def getLayName(SC_name,lay):
 if __name__ == '__main__':
 
     ROOT.gROOT.SetBatch(True)
-    
+
     # Define the parser
     import argparse
     from argparse import RawTextHelpFormatter
@@ -57,37 +57,37 @@ if __name__ == '__main__':
     parser.add_argument("runListFile", type=str, help="Please provide the full path to the desired file with list of runs to be used")
     parser.add_argument("xAxisVar", type=str, default="gain", choices=["gain","EDcurrent","Threshold"], help="Please specify the variable to be used for the x axis. Choices: \"gain\" or \"EDcurrent\" or \"Threshold\"")
     args = parser.parse_args()
-    
+
     try:
         infile = open(args.runListFile, 'rb')
     except IOError:
         sys.exit("Could not read file: " + args.runListFile)
-    
+
     maskCol = [None]*3
     runList,maskCol[0],maskCol[1],maskCol[2] = np.loadtxt(infile, delimiter="\t", skiprows=1, unpack=True)
-    
+
     infile.close()
-    
+
     SCList = []
     vfatList = map(str, range(0,24))
- 
+
     for run in runList:
         RunLogsFolder =  "/data/bigdisk/GEM-Data-Taking/GE11_QC8/QC8_RunLogs/Run_{:06d}/".format(int(run))
         for logs in os.listdir(RunLogsFolder):
             chID = logs.split('.')[0].split('_')[2]
             if getSCname(chID) not in SCList:
                 SCList.append(getSCname(chID))
-                
+
     for SC in SCList:
-        ROOT.gStyle.SetPalette(ROOT.kRainBow)
-        c = ROOT.TCanvas("c","",100,100,1200,1200)
+        #ROOT.gStyle.SetPalette(ROOT.kRainBow)
+        c = ROOT.TCanvas('c','',200,10,2000,2000)
         c.SetGrid()
         #legend = ROOT.TLegend(0.15,0.15,0.85,0.4)
         legend = ROOT.TLegend(0.15,0.15,0.85,0.33)
         legend.SetHeader("#splitline{#splitline{}{Gas mixture: Ar/CO_{2} (70/30%)}}{#splitline{}{Super Chamber ID (Layer) == Chambers ID}}")
         x1, y1, xerr1, yerr1 = array.array( 'd' ), array.array( 'd' ), array.array( 'd' ), array.array( 'd' )
         x2, y2, xerr2, yerr2 = array.array( 'd' ), array.array( 'd' ), array.array( 'd' ), array.array( 'd' )
-        
+
         for layer in [1,2]:
             eff = 0.0
             eff_err = 0.0
@@ -100,14 +100,14 @@ if __name__ == '__main__':
                     print("Could not read file: " + chLogFile)
                     continue
                     #sys.exit("Could not read file: " + chLogFile)
-                
+
                 quantity,value = np.genfromtxt(chLog, dtype='str', delimiter="\t", unpack=True)
-            
+
                 row = int((str(value[np.where(quantity == "Position")]).split('/')[0])[-1])
                 column = int(str(value[np.where(quantity == "Position")]).split('/')[1])
-                
+
                 if (str(bin(int(maskCol[column-1][np.where(runList == run)]))[2:].zfill(12))[-(3+2*(row-1)+(layer-1))] == "1"): # if mask value is 1 for that chamber
-                    
+
                     if (args.xAxisVar == "gain" and layer == 1):
                         x1.append(float(value[np.where(quantity == "EffAvgGain")]))
                         xerr1.append(0.0)
@@ -119,7 +119,7 @@ if __name__ == '__main__':
                         thrList = value[boolVfat].astype(np.float) #list of threshold values for the 24 VFATs
                         x1.append(float(np.mean(thrList)))
                         xerr1.append(0.0)
-                        
+
                     if (args.xAxisVar == "gain" and layer == 2):
                         x2.append(float(value[np.where(quantity == "EffAvgGain")]))
                         xerr2.append(0.0)
@@ -131,61 +131,70 @@ if __name__ == '__main__':
                         thrList = value[boolVfat].astype(np.float) #list of threshold values for the 24 VFATs
                         x2.append(float(np.mean(thrList)))
                         xerr2.append(0.0)
-                        
+
                     resultsFile = "/home/gemuser/AnalysisQC8/Results_QC8_validation_run_" + str(int(run)) + "_yesMasks/Average_Efficiency_Per_Chamber.csv"
-                    
+
                     try:
                         results = open(resultsFile, 'rb')
                     except IOError:
                         print("Could not read file: " + resultsFile)
                         continue
                         #sys.exit("Could not read file: " + resultsFile)
-                    
+
                     PositionCMSSW,Position,Chamber,Efficiency,ErrorEfficiency = np.genfromtxt(results, dtype='str', delimiter=",", skip_header=1, unpack=True)
-                    
+
                     eff = float(Efficiency[np.where(Chamber == chName.replace('GE11-','GE1/1-'))])
                     eff_err = float(ErrorEfficiency[np.where(Chamber == chName.replace('GE11-','GE1/1-'))])
-                    
+
                     results.close()
-                    
+
                     """
-                    
+
                     resultsFile = "/home/gemuser/AnalysisQC8/Results_QC8_validation_run_" + str(int(run)) + "_noMasks/Average_Efficiency_Per_Chamber.csv"
-                    
+
                     try:
                         results = open(resultsFile, 'rb')
                     except IOError:
                         sys.exit("Could not read file: " + resultsFile)
-                    
+
                     PositionCMSSW,Position,Chamber,Efficiency,ErrorEfficiency = np.genfromtxt(results, dtype='str', delimiter=",", skip_header=1, unpack=True)
-                    
+
                     if (float(Efficiency[np.where(Chamber == chName.replace('GE11-','GE1/1-'))]) > eff):
                         eff = float(Efficiency[np.where(Chamber == chName.replace('GE11-','GE1/1-'))])
                         eff_err = float(ErrorEfficiency[np.where(Chamber == chName.replace('GE11-','GE1/1-'))])
-                    
+
                     results.close()
-                    
+
                     """
-                    
+
                     if (layer == 1):
                         y1.append(eff)
                         yerr1.append(eff_err)
                     elif (layer == 2):
                         y2.append(eff)
                         yerr2.append(eff_err)
-                    
+
                 chLog.close()
-                
-                
-        if (len(x1)==0 or len(x2)==0):
-            continue
-        
+
+
+        if (len(x1)==0):
+            x1.append(0.0)
+            xerr1.append(0.0)
+            y1.append(0.0)
+            yerr1.append(0.0)
+
+        if (len(x2)==0):
+            x2.append(0.0)
+            xerr2.append(0.0)
+            y2.append(0.0)
+            yerr2.append(0.0)
+
         print SC, runList, getLayName(SC,1), x1, y1, yerr1
         print SC, runList, getLayName(SC,2), x2, y2, yerr2
-                
+
         graph1 = ROOT.TGraphErrors(int(len(x1)), x1, y1, xerr1, yerr1)
         graph2 = ROOT.TGraphErrors(int(len(x2)), x2, y2, xerr2, yerr2)
-        
+
         graph1.SetTitle("Efficiency vs gain")
         if (args.xAxisVar == "gain"):
         	graph1.GetXaxis().SetRangeUser(6000, 23000)
@@ -195,53 +204,43 @@ if __name__ == '__main__':
         	graph1.GetXaxis().SetRangeUser(580, 720)
         	graph1.GetXaxis().SetTitle("Equivalent Divider Current (uA)")
         if (args.xAxisVar == "Threshold"):
-        	graph1.GetXaxis().SetRangeUser(1, 21)
+        	graph1.GetXaxis().SetRangeUser(1, 24)
         	graph1.GetXaxis().SetTitle("Average Threshold (fC)")
         graph1.GetXaxis().SetTitleOffset(1.1)
         graph1.GetXaxis().SetLabelSize(0.03)
         #graph1.GetYaxis().SetRangeUser(0.5, 1.0)
-        graph1.GetYaxis().SetRangeUser(-0.3, 1.0)
+        graph1.GetYaxis().SetRangeUser(0.0, 1.0)
         graph1.GetYaxis().SetDecimals()
         graph1.GetYaxis().SetLabelSize(0.03)
         graph1.GetYaxis().SetTitleOffset(1.3)
         graph1.GetYaxis().SetTitle("MIP Efficiency")
-        graph1.SetLineWidth(1)
+        graph1.SetMarkerStyle(20)
+        graph1.SetMarkerColor(ROOT.kRed)
+        graph1.SetLineWidth(2)
+        graph1.SetLineColor(ROOT.kRed)
         graph1.Sort()
-        graph1.Draw("ALP PLC PFC")
+        graph1.Draw("ALP")
         c.Update()
         namename = str(SC) + " (Layer1) == " + str(getLayName(SC,1))
         legend.AddEntry(graph1,namename,"l")
-        
-        graph2.SetLineWidth(1)
+
+        graph2.SetMarkerStyle(20)
+        graph2.SetMarkerColor(ROOT.kBlue)
+        graph2.SetLineWidth(2)
+        graph2.SetLineColor(ROOT.kBlue)
         graph2.Sort()
-        graph2.Draw("LP PLC PFC SAME")
+        graph2.Draw("LP SAME")
         c.Update()
         namename = str(SC) + " (Layer2) == " + str(getLayName(SC,2))
         legend.AddEntry(graph2,namename,"l")
-                
+
         legend.Draw("SAME")
-        
+
         latex = ROOT.TLatex()
         latex.SetNDC()
         latex.SetTextAngle(0)
         latex.SetTextColor(ROOT.kBlack)
         latex.SetTextFont(42)
         latex.DrawLatex(0.104,0.91,"#scale[0.5]{#bf{CMS} #it{Preliminary}}")
-        
+
         c.SaveAs(str(SC) + "_Eff" + args.xAxisVar + "Curve.png")
-            
-            
-            
-            
-            
-            
-            
-            
-                    
-        
-        
-        
-
-
-
-
